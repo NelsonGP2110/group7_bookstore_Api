b07106632cdca6bd0e816c308d3562fc1b623a9b
NelsonGP2110 <nelsongp603@hotmail.com>
2021-09-30 18:51:42 -0400

    Update book_api_controllers.js

--- src/controllers/book_api_controllers.js
+++ src/controllers/book_api_controllers.js
@@ -30,28 +30,6 @@ export const getBookByTitle = (req, res) => {
 /*this sends a request for the param genre , using a query to get the param and the key
 and then show the matches from the db.*/
 export const getBookByGenre = (req, res) => {
-  /*const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  const genre = readline.question(`Enter a genre: `, name => {
-    //midwares
-    // Cleaning console input.
-    let str = name.trim();
-    let str1 = str.toLowerCase();
-    let str2 = str1.charAt(0).toUpperCase() + str1.slice(1);
-
-    //console.log(`This is name: ${str2}`);
-   Books.find({ genre: { $all: [str2] } }, (err, books) => {
-      if (err) {
-        res.send(err);
-      }
-      res.json(books);
-    });
-    readline.close();
-
-  });*/
   //Cleaning query input.
   let genre = req.query.genre;
   let str = genre.trim();
