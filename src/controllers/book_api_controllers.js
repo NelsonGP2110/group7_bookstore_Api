4632739bdabf351c35e3268213586610939b06d1
NelsonGP2110 <nelsongp603@hotmail.com>
2021-10-02 23:11:45 -0400

    removing getBookByTitle
    it has the same functionality as getBook

--- src/controllers/book_api_controllers.js
+++ src/controllers/book_api_controllers.js
@@ -12,18 +12,6 @@ export const getBooks = (req, res) => {
   });
 };
 
-/*this send a request for the param title and send a response, 
-if pass responds with collection books by title, if dont throw an error*/
-// --WARNING-- This function is the same as getBooks
-export const getBookByTitle = (req, res) => {
-  Books.findById({ title }, (err, books) => {
-    if (err) {
-      res.send(err);
-    }
-    res.json(books);
-  });
-};
-
 //----------------------- NELSON FEATURE-------------------------//
 //-----------------GET A LIST OF BOOKS BY GENRE-----------------//
 
--- src/routes/server_routes.js
+++ src/routes/server_routes.js
@@ -1,5 +1,4 @@
 import {
-  //getBookByTitle, // No need for this. It is the same as getBooks
   getBooks,
   getBookByGenre,
   getBookTop10,
@@ -8,14 +7,7 @@ import {
 } from '../controllers/book_api_controllers';
 
 const routes = app => {
-  app.route('/books').get((req, res, next) => {
-    //middleware
-    console.log(`Request from: ${req.originalUrl}`);
-    console.log(`Request type: ${req.method}`);
-    next();
-  }, getBooks);
-
-  //app.route('/books/title').get(getBookByTitle); // No need for this. It is the same as getBooks
+  app.route('/books').get(getBooks);
   app.route('/books/genre').get(getBookByGenre);
   app.route('/books/top10').get(getBookTop10);
   app.route('/books/ratingselection').get(getRating);
