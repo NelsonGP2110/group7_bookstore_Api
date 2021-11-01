c26195afcf234267f6aee94662db4a93a330737b
The7redarrow <90415741+The7redarrow@users.noreply.github.com>
2021-11-01 18:43:09 -0400

    books.js

---
+++ src/routes/books.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var reviewsRouter = require('./books/reviews');
+var listingsRouter = require('./books/listings');
+
+router.use('/:isbn/reviews',reviewsRouter);
+
+router.use('/', listingsRouter);
+
+module.exports = router;
