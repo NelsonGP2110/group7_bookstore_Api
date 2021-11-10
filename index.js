ba328b010e29fd6f6e21297d937148661eecaaa9
The7redarrow <90415741+The7redarrow@users.noreply.github.com>
2021-11-09 22:04:39 -0500

    fixing server

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ mongoose.connect(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //app.use(express.json());
-app.use("/user", userRoutes);
-app.use("/books/:isbn/reviews", reviewRoutes);
+
 
 routes(app);
 
